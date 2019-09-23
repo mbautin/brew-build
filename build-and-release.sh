d28c6f09a780d58deeb0aecc1e1dc770f2c65d7e
Mikhail Bautin <mbautin@users.noreply.github.com>
2019-09-22 18:23:59 -0700

    Select a specific directory to build archives in

--- build-and-release.sh
+++ build-and-release.sh
@@ -12,10 +12,15 @@ linuxbrew_dir=/opt/yugabyte-build/linuxbrew-versions/$top_dir_name
 mkdir -p "$linuxbrew_dir"
 touch "$linuxbrew_dir"/foo
 touch "$linuxbrew_dir"/far
-tar cvzf "$top_dir_name.tar.gz" "$linuxbrew_dir"
+archives_dir=/opt/yugabyte-build/linuxbrew-archives
+mkdir -p "$archives_dir"
+archive_name="$top_dir_name.tar.gz"
+archive_path="$archives_dir/$archive_name"
+cd "$archives_dir"
+tar cvzf "$archive_path" "$linuxbrew_dir"
 create_release_cmd=( hub release create "$tag" -m "Release $tag" )
 for f in *.tar.gz; do
-  create_release_cmd+=( -a "$PWD/$top_dir_name.tar.gz" )
+  create_release_cmd+=( -a "$archive_path" )
 done
 "${create_release_cmd[@]}"
 cd "$linuxbrew_dir"
